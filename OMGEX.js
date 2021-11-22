6a4d6aa64ce309b7dafeb5b7dc3be9f88809bae1
Dakota Chambers <dakotachambers@gmail.com>
2021-11-22 15:54:51 -0600

    feat: Change localtime div to span so its on the same line as location

--- OMGEX.js
+++ OMGEX.js
@@ -18,7 +18,7 @@ window.OMGEX = function (options) {
             timeDisplayOptions.second="2-digit";
         }
         let details = document.querySelector("#details");
-        details.innerHTML = details.innerHTML + `<div id="localtime"><i class="fas fa-clock"></i> ${new Date().toLocaleTimeString([], {timeZone: options.timezone, hour: timeDisplayOptions.hour, minute: timeDisplayOptions.minute, second: timeDisplayOptions.second})}</div>`;
+        details.innerHTML = details.innerHTML + `<span id="localtime"><i class="fas fa-clock"></i> ${new Date().toLocaleTimeString([], {timeZone: options.timezone, hour: timeDisplayOptions.hour, minute: timeDisplayOptions.minute, second: timeDisplayOptions.second})}</span>`;
         setInterval(() => {
             document.querySelector("#localtime").innerHTML = `<i class="fas fa-clock"></i> ${new Date().toLocaleTimeString([], {timeZone: options.timezone, hour: timeDisplayOptions.hour, minute: timeDisplayOptions.minute, second: timeDisplayOptions.second})}`
         }, delay);
