6d62b2111e723ee3b617f7ffb8f80676b74f828c
MatseVH <49431416+Matse2005@users.noreply.github.com>
2023-01-27 10:34:33 +0100

    Added css for details

--- OMGEX.js
+++ OMGEX.js
@@ -1,5 +1,14 @@
 window.OMGEX = function (options) {
   let version = "1.5.2";
+
+  var head = document.getElementsByTagName("head")[0];
+  var link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = "https://cdn.jsdelivr.net/gh/litdevs/omgex@latest/OMGEX.css";
+  link.media = "all";
+  head.appendChild(link);
+
   if (!options || Object.keys(options).length == 0)
     return console.error("OMGEX: No options provided");
   if (options.zodiac) {
